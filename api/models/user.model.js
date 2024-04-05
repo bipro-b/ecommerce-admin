a0422890138566fb7de209efa8723d6e24a938b4
bipro <biprocuet@gmail.com>
2024-04-05 09:03:21 +0600

    server side running complete

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import { Mongoose } from "mongoose";
+
+const userShcema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        uniques:true
+    },
+    usernumber:{
+        type:String,
+        required:true,
+        uniques:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    }
+
+},{timestamps:true})
+
+const User = mongoose.model('User',userShcema);
+export default User;
