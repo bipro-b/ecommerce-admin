28878fca0a6e91d3c110efe19a073d198b2193c0
bipro <biprocuet@gmail.com>
2024-04-05 09:18:52 +0600

    auth router and controller complete

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,91 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async(req,res,next)=>{
+    const {username,usernumber,password} = req.body;
+
+    const hashPassword = bcryptjs.hasSync(password,10);
+
+    const newUser = new User({username,usernumber,hashPassword});
+
+    try {
+
+        await newUser.save();
+        res.status(201).json({
+            message:"User created successfully.",
+            newUser
+        })
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { usernumber, password } = req.body;
+  
+    try {
+      const validUser = await User.findOne({ usernumber: usernumber });
+      if (!validUser) return next(errorHandler(404, "User not found"));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, "Wrong Credential."));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+  
+      //expires:new Date(Date.now()+24*60*60)
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const google = async (req, res, next) => {
+    try {
+      const user = await User.findOne({ usernumber: req.body.usernumber });
+      if (user) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = user._doc;
+        res
+          .cookie('access_token', token, { httpOnly: true })
+          .status(200)
+          .json(rest);
+      } else {
+        const generatedPassword =
+          Math.random().toString(36).slice(-8) +
+          Math.random().toString(36).slice(-8);
+        const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+        const newUser = new User({
+          username:
+            req.body.name.split(' ').join('').toLowerCase() +
+            Math.random().toString(36).slice(-4),
+          usernumber: req.body.usernumber,
+          password: hashedPassword,
+          avatar: req.body.photo,
+        });
+        await newUser.save();
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = newUser._doc;
+        res
+          .cookie('access_token', token, { httpOnly: true })
+          .status(200)
+          .json(rest);
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  
+  export const signOut =async(req,res,next)=>{
+    try {
+      res.clearCookie('access_token');
+      res.status(200).json('User has been logged out!')
+    } catch (error) {
+      next(error)
+    }
+  }
